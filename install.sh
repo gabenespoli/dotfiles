713f4b6f7f368cf26e085c5d5bf73340d3bcc7c2
Gabe Nespoli <gabenespoli@gmail.com>
2017-03-24 22:48:17 -0400

    use case to install different things; not usable yet

--- install.sh
+++ install.sh
@@ -1,53 +1,74 @@
 #!/bin/bash
 
-# script for installing symlinks from Dropbox to home folder
+# script for installing symlinks from this dotfiles folder to home folder
+# https://github.com/gabenespoli/dotfiles
 
-if [ -d "$HOME/bin" ]
-then echo "$HOME/bin already exists. No link was created."
-else ln -s $HOME/Dropbox/bin $HOME/bin
-        echo "Successfully linked bin"
-fi
+# get path of current dir
+dotfilesDir=`pwd`
 
-if [ -d "$HOME/dotfiles" ]
-then echo "$HOME/dotfiles already exists. No link was created."
-else ln -s $HOME/Dropbox/dotfiles $HOME/dotfiles
-        echo "Successfully linked dotfiles"
-fi
+# loop through input args, switch case to install different things
+for arg in $@
+do
+    case "$arg" in
+        bashrc )
+            if [ -f "$HOME/.bashrc" ]; then
+                echo "$HOME/.bashrc already exists. No link was created."
+            else
+               ln -s "${dotfilesDir}/bashrc $HOME/.bashrc"
+                echo "$HOME/.bashrc successfully linked"
+            fi
+            ;;
+        vim )
+            if [ -f "$HOME/.vimrc" ] || [ -d "$HOME/.vim" ] ; then
+                echo "$HOME/.vimrc or .vim/ already exist. No links were created."
+            else
+                ln -s "${dotfilesDir}/vimrc $HOME/.vimrc"
+                mkdir $HOME/.vim
+                    
+                declare -a dirs=("after" "colors" "ftplugin" "spell")
+                for dir in "${dirs[@]}"
+                do
+                    ln -s "${dotfilesDir}/vim/$dir $HOME/.vim/$dir"
+                done
+                echo "Successfully linked .vimrc and .vim/"
 
-if [ -f "$HOME/.bashrc" ]
-then echo "$HOME/.bashrc already exists. No link was created."
-else ln -s $HOME/Dropbox/dotfiles/bashrc $HOME/.bashrc
-        echo "Successfully linked bashrc"
-fi
+                echo "Installing Vundle and installing plugins..."
+                git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+                vim +PluginInstall +qall
+                echo "Done."
+            fi
+            ;;
+        tmux )
+            if [ -f "$HOME/.tmux.conf" ] || [ -d "$HOME/.tmux" ] ; then
+                echo "$HOME/.tmux.conf or .tmux/ already exist. No links were created."
+            else
+                ln -s "${dotfilesDir}/tmux.conf $HOME/.tmux.conf"
+                mkdir $HOME/.tmux
 
-if [ -f "$HOME/.inputrc" ]
-then echo "$HOME/.inputrc already exists. No link was created."
-else ln -s $HOME/Dropbox/dotfiles/inputrc $HOME/.inputrc
-        echo "Successfully linked inputrc"
-fi
+                declare -a dirs=("tmux-gmac.conf" "tmux-mac.conf" "tmux-reset.conf")
+                for dir in "${dirs[@]}"
+                do
+                    ln -s "${dotfilesDir}/vim/$dir $HOME/.vim/$dir"
+                done
+                echo "Successfully linked .vimrc and .vim/"
+
+                echo "Installing Tmux Plugin Manager (TPM)..."
+                git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+                tmux source $HOME/.tmux.conf
+            fi
+    esac
+done
 
 # install homebrew and brews
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install git
-brew install bash
-brew install tmux
-brew install reattach-to-user-namespace
-brew install vim
-brew install pandoc
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew install git
+#brew install bash
+#brew install tmux
+#brew install reattach-to-user-namespace
+#brew install vim
+#brew install pandoc
 
 # change shell to brew's version of bash
-sudo echo "/usr/local/bin/bash" >> /etc/shells
-sudo chsh -s /usr/local/bin/bash
+#sudo echo "/usr/local/bin/bash" >> /etc/shells
+#sudo chsh -s /usr/local/bin/bash
 
-# symlinks
-ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
-ln -s $HOME/dotfiles/tmux-mac.conf $HOME/.tmux-mac.conf
-
-# install tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-
-# install vim plugin manager (vundle)
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-ln -s $HOME/dotfiles/vim/ftplugin $HOME/.vim/ftplugin
-ln -s $HOME/dotfiles/vim/spell $HOME/.vim/spell
-vim +PluginInstall +qall
