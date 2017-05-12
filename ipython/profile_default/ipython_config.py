8fd7ffd843938c933c4d7c8f9b789148206e4696
Gabe Nespoli <gabenespoli@gmail.com>
2017-05-12 16:16:51 -0400

    trying very hard to get colours and show-mode-in-prompt working. holy hell.

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -6,45 +6,68 @@ c = get_config()
 #c.InteractiveShellApp.extensions = [
 #    'myextension'
 #]
-c.InteractiveShellApp.exec_lines = [
-    'import numpy as np',
-    'import scipy as sp',
-    'import pandas as pd'
-]
+#c.InteractiveShellApp.exec_lines = [
+#    'import numpy as np',
+#    'import scipy as sp',
+#    'import pandas as pd'
+#]
 #c.InteractiveShellApp.exec_files = [
     #'mycode.py',
     #'fancy.ipy'
 #]
 c.InteractiveShell.autoindent = True
 #c.InteractiveShell.colors = 'LightBG'
-#c.InteractiveShell.confirm_exit = False
+c.InteractiveShell.confirm_exit = False
 c.InteractiveShell.editor = 'vim'
 #c.InteractiveShell.xmode = 'Context'
 
 #c.PrefilterManager.multi_line_specials = True
 
-#c.AliasManager.user_aliases = [
-# ('la', 'ls -al')
-#]
-
-
+c.AliasManager.user_aliases = [
+ ('ls', 'ls -hl'),
+ ('la', 'ls -hla'),
+ ('lsa', 'ls -hla'),
+]
 
 c.TerminalInteractiveShell.editing_mode = 'vi'
+
+## Colours
+#from pygments.token import Token as pToken
+#c.TerminalInteractiveShell.highlighting_style_overrides = {
+#    pToken.Keyword: '#ansidarkgreen',
+#    pToken.Number: '#ansiturquoise',
+#    pToken.Operator: 'noinherit',
+#    pToken.String: '#ansiturquoise',
+#    pToken.Name.Function: '#ansidarkblue',
+#    pToken.Name.Class: '#ansidarkblue',
+#    pToken.Name.Namespace: '#ansidarkblue',
+#    pToken.Prompt: '#ansidarkblue',
+#    pToken.PromptNum: '#ansilightgray',
+#    pToken.OutPrompt: '#ansidarkblue',
+#    pToken.OutPromptNum: '#ansilightgray',
+#}
 #c.InteractiveShell.colors = 'Neutral'
 #c.TerminalInteractiveShell.highlighting_style = 'solarized256'
 
-from pygments.token import Token
+from base16 import Base16Style, overrides
+from IPython.terminal import interactiveshell
+def get_style_by_name(name, original=interactiveshell.get_style_by_name):
+    return Base16Style if name == 'base16' else original(name)
+interactiveshell.get_style_by_name = get_style_by_name
+c.TerminalInteractiveShell.highlighting_style = 'base16'
+c.TerminalInteractiveShell.highlighting_style_overrides = overrides
 
-c.TerminalInteractiveShell.highlighting_style_overrides = {
-    Token.Keyword: '#ansidarkgreen',
-    Token.Number: '#ansiturquoise',
-    Token.Operator: 'noinherit',
-    Token.String: '#ansiturquoise',
-    Token.Name.Function: '#ansidarkblue',
-    Token.Name.Class: '#ansidarkblue',
-    Token.Name.Namespace: '#ansidarkblue',
-    Token.Prompt: '#ansidarkblue',
-    Token.PromptNum: '#ansilightgray',
-    Token.OutPrompt: '#ansidarkblue',
-    Token.OutPromptNum: '#ansilightgray',
-}
+
+## Prompt
+from IPython.terminal.prompts import Prompts, Token
+from prompt_toolkit.key_binding.vi_state import InputMode
+import os
+
+class MyPrompt(Prompts):
+     def in_prompt_tokens(self, cli=None):
+         return [(Token.PromptWhoKnows, '? '),
+                 (Token.PromptPath, os.getcwd()),
+                 (Token.Prompt, ' >>> ')]
+
+c.TerminalInteractiveShell.prompts_class = MyPrompt
+
