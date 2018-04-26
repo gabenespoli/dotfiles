9d17fe109dab236800909d2a313a09b7e31b2f3f
Gabe Nespoli <gabenespoli@gmail.com>
2018-04-26 13:49:03 -0400

    new install.sh for ubuntu 16.04 reinstall

--- install.sh
+++ install.sh
@@ -1,136 +1,57 @@
 #!/bin/bash
 
-# script for installing symlinks from this dotfiles folder to home folder
-# https://github.com/gabenespoli/dotfiles
+# get this file on your computer first:
+# cd $HOME
+# sudo apt-get -y install openssh-server
+# sudo apt-get -y install git
+# git clone https://github.com/gabenespoli/dotfiles
 
-# path to dotfiles dir
-dotfiles="$HOME/dotfiles"
+# xterm with italics
+# https://medium.com/@dubistkomisch/how-to-actually-get-italics-and-true-colour-to-work-in-iterm-tmux-vim-9ebe55ebc2be
+tic -x $HOME/dotfiles/xterm-256color-italics.terminfo
+tic -x $HOME/dotfiles/tmux-256color.terminfo
 
-# loop through input args, switch case to install different things
-for arg in $@
-do
-    case "$arg" in
-        bashrc )
-            mv "$HOME/.bashrc $HOME/.bashrc.bak"
-            ln -s "$dotfiles/bashrc $HOME/.bashrc"
-            ln -s "$dotfiles/bash_profile $HOME/.bash_profile"
-            ln -s "$dotfiles/inputrc $HOME/.inputrc"
-            ;;
+# bash
+mv $HOME/.bashrc $HOME/.bashrc.bak
+ln -s $HOME/dotfiles/bashrc $HOME/.bashrc
+ln -s $HOME/dotfiles/inputrc $HOME/.inputrc
+source $HOME/.bashrc
+sudo apt-get -y install curl
 
-        vim )
-            #vim
-            ln -s "$dotfiles/vimrc $HOME/.vimrc"
-            mkdir $HOME/.vim
-            declare -a dirs=("after" "colors" "ftplugin" "spell" "ftdetect" "syntax" "autoload")
-            for dir in "${dirs[@]}"
-            do
-                ln -s "$dotfiles/vim/$dir $HOME/.vim/$dir"
-            done
-            # nvim
-            mkdir -p $HOME/.config/nvim
-            ln -s "$dotfiles/vimrc $HOME/.config/nvim/init.vim"
-            mkdir -p $HOME/.local/share/nvim
-            ln -s "$HOME/.vim $HOME/.local/share/nvim/site"
-            ;;
+# vim
+sudo apt-get -y install vim
+ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
+mkdir $HOME/.vim
+ln -s $HOME/dotfiles/vim/autoload $HOME/.vim/autoload
+ln -s $HOME/dotfiles/vim/ftdetect $HOME/.vim/ftdetect
+ln -s $HOME/dotfiles/vim/ftplugin $HOME/.vim/ftplugin
+ln -s $HOME/dotfiles/vim/spell $HOME/.vim/spell
+ln -s $HOME/dotfiles/vim/syntax $HOME/.vim/syntax
 
-        tmux )
-            ln -s "$dotfiles/tmux.conf $HOME/.tmux.conf"
-            mkdir $HOME/.tmux
-            ln -s "$dotfiles/tmux/tmux-mac.conf $HOME/.tmux/.tmux-mac.conf"
-            ln -s "$dotfiles/tmux/tmux-linux.conf $HOME/.tmux/.tmux-linux.conf"
-            git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-            ;;
+# neovim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+mkdir -p $HOME/.config/nvim
+ln -s "$dotfiles/vimrc $HOME/.config/nvim/init.vim"
+mkdir -p $HOME/.local/share/nvim
+ln -s "$HOME/.vim $HOME/.local/share/nvim/site"
 
-        todo )
-            ln -s "$HOME/Dropbox/todo $HOME/todo"
-            ln -s "$dotfiles/todo.cfg $HOME/.todo.cfg"
-            ln -s "$dotfiles/todo.actions.d $HOME/.todo.actions.d"
-            ;;
+# tmux
+sudo apt-get -y install tmux
+ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
+mkdir $HOME/.tmux
+# ln -s $HOME/dotfiles/tmux/tmux-mac.conf $HOME/.tmux/tmux-mac.conf
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
-        folders )
-            ln -s "$HOME/Dropbox/bin $HOME/bin"
-            ln -s "$HOME/Dropbox/bin/private $HOME/private"
-            ln -s "$HOME/Dropbox/projects $HOME/projects"
-            mkdir "$HOME/local"
-            ;;
+# ranger
+mkdir $HOME/local
+git clone https://github.com/ranger/ranger "$HOME/local"
 
-        pandoc )
-            ln -s "$dotfiles/pandoc $HOME/.pandoc"
-            ;;
+# misc
+sudo apt-get -y install tig
+ln -s $HOME/dotfiles/tigrc $HOME/.tigrc
 
-        coding )
-            ln -s "$dotfiles/gitconfig $HOME/.gitconfig"
-            ln -s "$dotfiles/octaverc $HOME/.octaverc"
-            ln -s "$dotfiles/Rprofile $HOME/.Rprofile"
-            ln -s "$dotfiles/ipython $HOME/.ipython"
-            ln -s "$dotfiles/highlight $HOME/.highlight"
-            ;;
+# install linuxbrew
+# brew install fzf
 
-        email )
-            ln -s "$dotfiles/muttrc $HOME/.muttrc"
-            ln -s "$dotfiles/mutt $HOME/.mutt"
-            ln -s "$dotfiles/msmtprc $HOME/.msmtprc"
-            ln -s "$HOME/bin/private/offlineimaprc $HOME/.msmtprc"
-            ln -s "$HOME/bin/private/goobookrc $HOME/.goobookrc"
-            ;;
-
-        mac )
-            # install homebrew
-            /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-            brew install git
-            brew install bash
-            brew install vim
-            brew install tmux
-            brew install reattach-to-user-namespace
-            brew install ranger
-
-            brew install python
-            brew install python3
-            brew install octave
-            brew install r
-            brew install rbenv
-
-            pip2 install ipython scipy numpy pandas matplotlib scikit-learn
-
-            brew install mutt
-            brew install offlineimap
-            brew install msmtp
-            brew install urlview
-            pip2 install urlscan
-            pip install goobook
-
-            brew install pandoc
-            brew install pandoc-citeproc
-
-            brew install rlwrap
-            brew install ctags
-            pip3 install hangups
-
-            # change shell to brew's version of bash
-            sudo echo "/usr/local/bin/bash" >> /etc/shells
-            sudo chsh -s /usr/local/bin/bash
-            ;;
-
-        linux )
-            sudo apt-get install git
-            sudo apt-get install vim
-            sudo apt-get install nvim
-            sudo apt-get install tmux
-            git clone https://github.com/ranger/ranger "$HOME/local"
-
-            sudo apt-get install r-base
-            sudo apt-get install octave
-            sudo apt-get install rbenv
-            sudo apt-get install ipython
-            sudo apt-get install python-pip
-            pip install scipy numpy pandas matplotlib scikit-learn
-
-            sudo apt-get install pandoc
-            sudo apt-get install pandoc-citeproc
-            sudo apt-get install ctags
-            sudo apt-get install rlwrap
-            ;;
-
-    esac
-done
