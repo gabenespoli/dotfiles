9a0d70e0d8402e816891ef94d20b329c3c2d8600
Gabe Nespoli <gabenespoli@gmail.com>
2018-04-26 15:09:43 -0400

    install.sh: add installing R

--- install.sh
+++ install.sh
@@ -64,3 +64,10 @@ cd $HOME
 sudo apt-get -y install tig
 ln -s $HOME/dotfiles/tigrc $HOME/.tigrc
 
+# R
+# https://www.r-bloggers.com/how-to-install-r-on-linux-ubuntu-16-04-xenial-xerus/
+sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.list
+gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
+gpg -a --export E084DAB9 | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install r-base
