1e50544e3a8bd1ed8dfcc43c1d61f37cd7d2b136
Gabe Nespoli <gabenespoli@gmail.com>
2018-04-26 22:40:26 -0400

    install.sh: add headings

--- install.sh
+++ install.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
+## git
 # get this file on your computer first:
 # cd $HOME
 # sudo apt-get -y install git
 # git clone https://github.com/gabenespoli/dotfiles
 ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
+sudo apt-get -y install tig
+ln -s $HOME/dotfiles/tigrc $HOME/.tigrc
 
-# xterm with italics
+## xterm with italics
 # https://medium.com/@dubistkomisch/how-to-actually-get-italics-and-true-colour-to-work-in-iterm-tmux-vim-9ebe55ebc2be
 tic -x $HOME/dotfiles/xterm-256color-italics.terminfo
 tic -x $HOME/dotfiles/tmux-256color.terminfo
 
-# folders
+## folders
 mkdir $HOME/local
 sudo apt-get -y install openssh-server
 
-# bash
+## bash
 mv $HOME/.bashrc $HOME/.bashrc.bak
 ln -s $HOME/dotfiles/bashrc $HOME/.bashrc
 ln -s $HOME/dotfiles/inputrc $HOME/.inputrc
 source $HOME/.bashrc
 sudo apt-get -y install curl
 
-# dropbox
+## dropbox
 sudo apt-get -y install nautilus-dropbox
 dropbox start -i
 
-# vim
+## vim
 sudo apt-get -y install vim
 ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
 mkdir $HOME/.vim
@@ -38,7 +41,7 @@ ln -s $HOME/dotfiles/vim/syntax $HOME/.vim/syntax
 # TODO can't find colorscheme solarized because it hasn't been downloaded yet
 vim -c "PlugInstall" -c "qall"
 
-# neovim
+## neovim
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get -y install neovim
@@ -47,14 +50,14 @@ ln -s $HOME/dotfiles/vimrc $HOME/.config/nvim/init.vim
 mkdir -p $HOME/.local/share/nvim
 ln -s $HOME/.vim $HOME/.local/share/nvim/site
 
-# tmux
+## tmux
 sudo apt-get -y install tmux
 ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 mkdir $HOME/.tmux
 # ln -s $HOME/dotfiles/tmux/tmux-mac.conf $HOME/.tmux/tmux-mac.conf
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
-# ranger
+## ranger
 ln -s ~/dotfiles/config/ranger ~/.config/ranger
 cd $HOME/local
 git clone https://github.com/ranger/ranger
@@ -62,11 +65,7 @@ cd ranger
 sudo make install
 cd $HOME
 
-# tig
-sudo apt-get -y install tig
-ln -s $HOME/dotfiles/tigrc $HOME/.tigrc
-
-# R
+## R
 # https://www.r-bloggers.com/how-to-install-r-on-linux-ubuntu-16-04-xenial-xerus/
 sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.list
 gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
