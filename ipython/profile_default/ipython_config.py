bc837c850107617574fbba4dfeb4a59e114322f1
Gabe Nespoli <gabenespoli@gmail.com>
2018-11-01 16:43:42 -0400

    update ipython config incl colors

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -1,57 +1,55 @@
-# sample ipython_config.py
+"""My ipython config (gabenespoli@gmail.com)"""
 c = get_config()
 
 #c.TerminalIPythonApp.display_banner = True
 #c.InteractiveShellApp.log_level = 20
-c.InteractiveShellApp.extensions = [
-    'autoreload'
-]
-c.InteractiveShellApp.exec_lines = [
-    '%autoreload 2'
-]
+# c.InteractiveShellApp.extensions = [
+    # 'autoreload'
+# ]
+# c.InteractiveShellApp.exec_lines = [
+    # '%autoreload 2'
+# ]
 #c.InteractiveShellApp.exec_files = [
     #'mycode.py',
     #'fancy.ipy'
 #]
 c.InteractiveShell.autoindent = True
-#c.InteractiveShell.colors = 'LightBG'
 c.InteractiveShell.confirm_exit = False
-c.InteractiveShell.editor = 'vim'
-#c.InteractiveShell.xmode = 'Context'
+# c.InteractiveShell.editor = 'vim'
+# c.TerminalInteractiveShell.editing_mode = 'vi'
+# c.InteractiveShell.xmode = 'Context'
 
 #c.PrefilterManager.multi_line_specials = True
 
-c.AliasManager.user_aliases = [
-    ('ls', 'ls -hl'),
-    ('la', 'ls -hla'),
-    ('lsa', 'ls -hla'),
-]
+# c.AliasManager.user_aliases = [
+#     ('ls', 'ls -hl'),
+#     ('la', 'ls -hla'),
+#     ('lsa', 'ls -hla'),
+# ]
 
-c.TerminalInteractiveShell.editing_mode = 'vi'
-
-## Colours
-#from pygments.token import Token as pToken
-#c.TerminalInteractiveShell.highlighting_style_overrides = {
-#    pToken.Keyword: '#ansidarkgreen',
-#    pToken.Number: '#ansiturquoise',
-#    pToken.Operator: 'noinherit',
-#    pToken.String: '#ansiturquoise',
-#    pToken.Name.Function: '#ansidarkblue',
-#    pToken.Name.Class: '#ansidarkblue',
-#    pToken.Name.Namespace: '#ansidarkblue',
-#    pToken.Prompt: '#ansidarkblue',
-#    pToken.PromptNum: '#ansilightgray',
-#    pToken.OutPrompt: '#ansidarkblue',
-#    pToken.OutPromptNum: '#ansilightgray',
-#}
-#c.InteractiveShell.colors = 'Neutral'
-#c.TerminalInteractiveShell.highlighting_style = 'solarized256'
-
-from base16 import Base16Style, overrides
-from IPython.terminal import interactiveshell
-def get_style_by_name(name, original=interactiveshell.get_style_by_name):
-    return Base16Style if name == 'base16' else original(name)
-interactiveshell.get_style_by_name = get_style_by_name
-c.TerminalInteractiveShell.highlighting_style = 'base16'
-c.TerminalInteractiveShell.highlighting_style_overrides = overrides
-
+# Colours
+c.InteractiveShell.colors = 'nocolor'
+from pygments.token import Token as pToken
+c.TerminalInteractiveShell.highlighting_style_overrides = {
+    pToken.Text: '#ansiblue',
+    pToken.Error: '#ansired',
+    pToken.Comment: '#ansigreen',
+    pToken.Keyword: '#ansidarkgreen',
+    pToken.Keyword.Constant: '#ansiteal',
+    pToken.Keyword.Namespace: '#ansiturquoise',
+    pToken.Name.Builtin: '#ansidarkblue',
+    pToken.Name.Function: '#ansidarkblue',
+    pToken.Name.Class: '#ansidarkblue',
+    pToken.Name.Decorator: '#ansired',
+    pToken.Name.Exception: '#ansired',
+    pToken.Name.Namespace: '#ansidarkblue',
+    pToken.Number: '#ansiteal',
+    pToken.String: '#ansiteal',
+    pToken.Operator: 'noinherit',
+    pToken.Operator.Word: '#ansigreen',
+    pToken.Literal: '#ansiteal',
+    pToken.Prompt: '#ansidarkblue',
+    pToken.PromptNum: '#ansidarkblue',
+    pToken.OutPrompt: '#ansiturquoise',
+    pToken.OutPromptNum: '#ansiturquoise',
+}
