fe0017218d15d2111d5766bed6c9783877ef3503
Gabe Nespoli <gabenespoli@gmail.com>
2018-11-25 20:57:03 -0500

    total revamp of ipython config

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -1,55 +1,145 @@
 """My ipython config (gabenespoli@gmail.com)"""
 c = get_config()
 
-#c.TerminalIPythonApp.display_banner = True
-#c.InteractiveShellApp.log_level = 20
-# c.InteractiveShellApp.extensions = [
-    # 'autoreload'
-# ]
-# c.InteractiveShellApp.exec_lines = [
-    # '%autoreload 2'
-# ]
-#c.InteractiveShellApp.exec_files = [
-    #'mycode.py',
-    #'fancy.ipy'
-#]
 c.InteractiveShell.autoindent = True
 c.InteractiveShell.confirm_exit = False
-# c.InteractiveShell.editor = 'vim'
-# c.TerminalInteractiveShell.editing_mode = 'vi'
-# c.InteractiveShell.xmode = 'Context'
-
-#c.PrefilterManager.multi_line_specials = True
-
-# c.AliasManager.user_aliases = [
-#     ('ls', 'ls -hl'),
-#     ('la', 'ls -hla'),
-#     ('lsa', 'ls -hla'),
-# ]
+c.InteractiveShell.editor = 'nvim'
+c.TerminalInteractiveShell.editing_mode = 'vi'
 
 # Colours
 c.InteractiveShell.colors = 'nocolor'
-from pygments.token import Token as pToken
+
+# base16 color maps
+bg = '#ansiblack'          # 0
+bg_light = '#ansigreen'    # 10
+bg_sel = '#ansiyellow'     # 11
+fg_com = '#ansidarkgray'   # 8
+fg_dark = '#ansiblue'      # 12
+fg = '#ansilightgray'      # 7
+fg_light = '#ansifuchsia'  # 13
+fg_bright = '#ansiwhite'   # 15
+red = '#ansidarkred'       # 1
+orange = '#ansired'        # 9
+yellow = '#ansibrown'      # 3
+green = '#ansidarkgreen'   # 2
+cyan = '#ansiteal'         # 6
+blue = '#ansidarkblue'     # 4
+purple = '#ansipurple'     # 5
+pink = '#ansiturquoise'    # 14
+
+from pygments.style import Style
+from pygments.token import Keyword, Name, Comment, String, Error, Text, \
+     Number, Operator, Generic, Whitespace, Punctuation, Other, Literal
+
+class base16_sumach(Style):
+
+    default_style = ''
+
+    background_color = bg
+    highlight_color = bg_sel
+
+    styles = {
+        # No corresponding class for the following:
+        # Text:                      fg,
+
+        Comment:                   fg_com,      # class: 'c'
+        Error:                     red,
+
+        Keyword:                   blue,
+        Keyword.Type:              yellow,
+        Keyword.Namespace:         purple,
+
+        Operator:                  fg,        # class: 'o'
+
+        # Punctuation:               fg,          # class: 'p'
+
+        Name:                      fg,          # class: 'n'
+        Name.Attribute:            blue,        # class: 'na' - to be revised
+        # Name.Builtin:              "",          # class: 'nb'
+        # Name.Builtin.Pseudo:       "",          # class: 'bp'
+        Name.Class:                yellow,      # class: 'nc' - to be revised
+        # Name.Constant:             cyan,         # class: 'no' - to be revised
+        # Name.Decorator:            cyan,        # class: 'nd' - to be revised
+        # Name.Entity:               "",          # class: 'ni'
+        # Name.Exception:            red,         # class: 'ne'
+        Name.Function:             green,        # class: 'nf'
+        # Name.Property:             "",          # class: 'py'
+        # Name.Label:                "",          # class: 'nl'
+        # Name.Namespace:            yellow,      # class: 'nn' - to be revised
+        # Name.Other:                blue,        # class: 'nx'
+        # Name.Tag:                  cyan,        # class: 'nt' - like a keyword
+        # Name.Variable:             red,         # class: 'nv' - to be revised
+        # Name.Variable.Class:       "",          # class: 'vc' - to be revised
+        # Name.Variable.Global:      "",          # class: 'vg' - to be revised
+        # Name.Variable.Instance:    "",          # class: 'vi' - to be revised
+
+        Number:                    cyan,
+
+        # Literal:                   orange,    # class: 'l'
+        # Literal.Date:              green,     # class: 'ld'
+
+        String:                    cyan,       # class: 's'
+        # String.Backtick:           "",          # class: 'sb'
+        # String.Char:               fg,  # class: 'sc'
+        # String.Doc:                fg_com,     # class: 'sd' - like a comment
+        # String.Double:             "",          # class: 's2'
+        String.Escape:             orange,      # class: 'se'
+        # String.Heredoc:            "",          # class: 'sh'
+        # String.Interpol:           orange,      # class: 'si'
+        # String.Other:              "",          # class: 'sx'
+        # String.Regex:              "",          # class: 'sr'
+        # String.Single:             "",          # class: 's1'
+        # String.Symbol:             "",          # class: 'ss'
+
+        # Generic:                   "",                    # class: 'g'
+        # Generic.Deleted:           red,                   # class: 'gd',
+        # Generic.Emph:              "italic",              # class: 'ge'
+        # Generic.Error:             "",                    # class: 'gr'
+        # Generic.Heading:           "bold " + fg,  # class: 'gh'
+        # Generic.Inserted:          green,                 # class: 'gi'
+        # Generic.Output:            "",                    # class: 'go'
+        # Generic.Prompt:            blue,     # class: 'gp'
+        # Generic.Strong:            "bold",                # class: 'gs'
+        # Generic.Subheading:        "bold " + cyan,        # class: 'gu'
+        # Generic.Traceback:         "",                    # class: 'gt'
+
+
+    }
+
+c.TerminalInteractiveShell.highlighting_style = base16_sumach
+
+from pygments.token import Token
 c.TerminalInteractiveShell.highlighting_style_overrides = {
-    pToken.Text: '#ansiblue',
-    pToken.Error: '#ansired',
-    pToken.Comment: '#ansigreen',
-    pToken.Keyword: '#ansidarkgreen',
-    pToken.Keyword.Constant: '#ansiteal',
-    pToken.Keyword.Namespace: '#ansiturquoise',
-    pToken.Name.Builtin: '#ansidarkblue',
-    pToken.Name.Function: '#ansidarkblue',
-    pToken.Name.Class: '#ansidarkblue',
-    pToken.Name.Decorator: '#ansired',
-    pToken.Name.Exception: '#ansired',
-    pToken.Name.Namespace: '#ansidarkblue',
-    pToken.Number: '#ansiteal',
-    pToken.String: '#ansiteal',
-    pToken.Operator: 'noinherit',
-    pToken.Operator.Word: '#ansigreen',
-    pToken.Literal: '#ansiteal',
-    pToken.Prompt: '#ansidarkblue',
-    pToken.PromptNum: '#ansidarkblue',
-    pToken.OutPrompt: '#ansiturquoise',
-    pToken.OutPromptNum: '#ansiturquoise',
+    Token.Prompt: blue,
+    Token.PromptNum: blue,
+    Token.OutPrompt: pink,
+    Token.OutPromptNum: pink,
 }
+
+# change cursor shape based on vi mode
+# https://stackoverflow.com/questions/44191529/changing-the-cursor-shape-in-the-interactive-ipython-consle-depending-on-vi-mode
+import sys
+from prompt_toolkit.key_binding.vi_state import InputMode, ViState
+
+def get_input_mode(self):
+    return self._input_mode
+
+def set_input_mode(self, mode):
+    shape = {InputMode.NAVIGATION: 1, InputMode.REPLACE: 3}.get(mode, 5)
+    raw = u'\x1b[{} q'.format(shape)
+    if hasattr(sys.stdout, '_cli'):
+        out = sys.stdout._cli.output.write_raw
+    else:
+        out = sys.stdout.write
+    out(raw)
+    sys.stdout.flush()
+    self._input_mode = mode
+
+ViState._input_mode = InputMode.INSERT
+ViState.input_mode = property(get_input_mode, set_input_mode)
+c.TerminalInteractiveShell.editing_mode = 'vi'
+
+# remove vi mode from prompt
+# had to manually edit ipython package per this commit:
+# https://github.com/ipython/ipython/pull/11492/commits/77a6963866763e1cca0b1091b1b4d50d1e11e524
+c.TerminalInteractiveShell.prompt_includes_vi_mode = False
