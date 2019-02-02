cc38f21e3d5eed167ec2053f80101f4c39441af7
Gabe Nespoli <gabenespoli@gmail.com>
2019-02-02 09:14:51 -0500

    ipython: use emacs bindings in shell

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -4,7 +4,7 @@ c = get_config()
 c.InteractiveShell.autoindent = False
 c.InteractiveShell.confirm_exit = False
 c.InteractiveShell.editor = 'nvim'
-c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.editing_mode = 'emacs'
 
 # Colours
 c.InteractiveShell.colors = 'nocolor'
@@ -115,29 +115,6 @@ c.TerminalInteractiveShell.highlighting_style_overrides = {
     Token.OutPrompt: pink,
     Token.OutPromptNum: pink,
 }
-
-# change cursor shape based on vi mode
-# https://stackoverflow.com/questions/44191529/changing-the-cursor-shape-in-the-interactive-ipython-consle-depending-on-vi-mode
-import sys
-from prompt_toolkit.key_binding.vi_state import InputMode, ViState
-
-def get_input_mode(self):
-    return self._input_mode
-
-def set_input_mode(self, mode):
-    shape = {InputMode.NAVIGATION: 1, InputMode.REPLACE: 3}.get(mode, 5)
-    raw = u'\x1b[{} q'.format(shape)
-    if hasattr(sys.stdout, '_cli'):
-        out = sys.stdout._cli.output.write_raw
-    else:
-        out = sys.stdout.write
-    out(raw)
-    sys.stdout.flush()
-    self._input_mode = mode
-
-ViState._input_mode = InputMode.INSERT
-ViState.input_mode = property(get_input_mode, set_input_mode)
-c.TerminalInteractiveShell.editing_mode = 'vi'
 
 # remove vi mode from prompt
 # had to manually edit ipython package per this commit:
