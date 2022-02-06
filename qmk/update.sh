c53ac0c0186e771d162be63a6b6df523b4599d11
Gabe Nespoli <gabe.nespoli@sonova.com>
2022-02-06 05:44:40 -0500

    qmk/update.sh: Add -v to mv

--- qmk/update.sh
+++ qmk/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/dotfiles/qmk
-mv ~/Downloads/massdrop_carina_c_Carina_20399.bin .
+mv -v ~/Downloads/massdrop_carina_c_Carina_20399.bin .
 ./mdloader_mac --first --download massdrop_carina_c_Carina_20399.bin --restart   
 # press <Fn>b
 cd -
