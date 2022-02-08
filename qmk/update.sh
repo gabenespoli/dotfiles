b22aa64021946b815347b17f6fbf38319788acfc
Gabe Nespoli <gabenespoli@gmail.com>
2022-02-08 04:56:01 -0500

    qmk/update.sh: Use $HOME not ~

--- qmk/update.sh
+++ qmk/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd ~/dotfiles/qmk
-mv -v ~/Downloads/massdrop_carina_c_Carina_20399.bin .
+cd "$HOME"/dotfiles/qmk
+mv -v "$HOME"/Downloads/massdrop_carina_c_Carina_20399.bin .
 ./mdloader_mac --first --download massdrop_carina_c_Carina_20399.bin --restart   
 # press <Fn>b
 cd -
