e092a890ff354d464e769a7136372063a0a212e2
Gabe Nespoli <gabenespoli@gmail.com>
2022-02-08 04:56:40 -0500

    Refactor install/python_envs.sh

--- install/python_envs.sh
+++ install/python_envs.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-echo "" && echo "-- Installing python envs..."
-
+echo "" && echo "-- Linking python config..."
+mkdir -pv "$HOME"/.ipython/profile_default
+ln -sfv "$HOME"/dotfiles/python/ipython_config.py "$HOME"/.ipython/profile_default/ipython_config.py
 ln -sfv "$HOME"/dotfiles/python/isort.cfg "$HOME"/.isort.cfg
 ln -sfv "$HOME"/dotfiles/python/flake8 "$HOME"/.flake8
 
-mkdir -pv "$HOME"/.ipython/profile_default
-ln -sfv "$HOME"/dotfiles/python/ipython_config.py "$HOME"/.ipython/profile_default/ipython_config.py
-
+echo "" && echo "-- Installing miniconda and python environments..."
 # https://docs.conda.io/projects/conda/en/latest/user-guide/install/macos.html
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O "$HOME"/miniconda.sh
 bash "$HOME"/miniconda.sh -b -p $HOME/miniconda3
-rm "$HOME"/miniconda.sh
+rm -v "$HOME"/miniconda.sh
 conda init
 conda update -n base -c defaults conda
 
-# env for neovim
 conda create -y -n neovim3
 conda deactivate && conda activate neovim3
 conda install -y python
