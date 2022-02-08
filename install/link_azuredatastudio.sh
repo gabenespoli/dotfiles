189800655d431468ca502c38bc2ff4bc95bdb332
Gabe Nespoli <gabe.nespoli@sonova.com>
2022-02-07 20:40:04 -0500

    Move azuredatastudio settings to misc/

--- install/link_azuredatastudio.sh
+++ install/link_azuredatastudio.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ln -sfv "$HOME"/dotfiles/azuredatastudio/settings.json \
+ln -sfv "$HOME"/dotfiles/misc/azuredatastudio/settings.json \
   "$HOME"/Library/Application\ Support/azuredatastudio/User/settings.json
