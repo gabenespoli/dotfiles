988ab6f71d07f441ae14cbcd2656a3baca1aedc7
Gabe Nespoli <gabenespoli@gmail.com>
2022-05-29 21:49:02 -0400

    qmk/update.sh: Update carina bin file

--- qmk/update.sh
+++ qmk/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$HOME"/dotfiles/qmk
-mv -v "$HOME"/Downloads/massdrop_carina_c_Carina_20399.bin .
-./mdloader_mac --first --download massdrop_carina_c_Carina_20399.bin --restart   
+mv -v "$HOME"/Downloads/massdrop_carina_c_Carina_Clea_33743.bin .
+./mdloader_mac --first --download massdrop_carina_c_Carina_Clea_33743.bin --restart   
 # press <Fn>b
 cd -
