f46472d9e6293ca5e4e036068fa6f4c214118376
Gabe Nespoli <gabe.nespoli@sonova.com>
2023-10-22 12:11:09 -0400

    Move install to install/main.sh

--- install/change_shell.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "-- Adding updated bash to the list of allowed shells..."
-sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells' # Prompts for password
-chsh -s /usr/local/bin/bash # Change to the new shell, prompts for password
